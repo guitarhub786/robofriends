dc7404da2abbb8eb738dd99ca39b1b3ef0342034
Guitarhub786 <codewithdrawal@gmail.com>
2019-10-30 18:14:40 +0000

    Updates

---
+++ precache-manifest.9f8ea9ffe6600dce0e81d39443e7923c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b28018890346b5472774f4c6b0de87",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8a18c3735eedd56b4adb",
+    "url": "/robofriends/static/css/main.b03b0c29.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "8a18c3735eedd56b4adb",
+    "url": "/robofriends/static/js/main.ea345e29.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
